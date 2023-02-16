fd37052bf53689cd634ca07c8bdb3d93c29670ab
KDhruv2608 <dhruvkatrodiya1234@gmail.com>
2023-02-16 10:59:33 +0530

    show table

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import './App.css';
 
-const app = () => {
+const App = () => {
   const cart = [
-        {name: "mobail",price:13000,quantity:4,discripion:"best mobail qulity"},
-        {name: "tv",price:25000,quantity:3,discripion:"best tv qulity"},
-        {name: "freez",price:16000,quantity:2,discripion:"best frrez qulity"},
-        {name: "ac",price:5765,quantity:4,discripion:"best ac qulity"},
-        {name: "cloths",price:7543,quantity:9,discripion:"best cloths qulity"},
-        {name: "bike",price:36646,quantity:1,discripion:"best bike qulity"}
-]
-console.info("cart++",cart)
+    { name: "mobail", price: 13000, quantity: 4, discripion: "best mobail qulity" },
+    { name: "tv", price: 25000, quantity: 3, discripion: "best tv qulity" },
+    { name: "freez", price: 16000, quantity: 2, discripion: "best frrez qulity" },
+    { name: "ac", price: 5765, quantity: 4, discripion: "best ac qulity" },
+    { name: "cloths", price: 7543, quantity: 9, discripion: "best cloths qulity" },
+    { name: "bike", price: 36646, quantity: 1, discripion: "best bike qulity" }
+  ]
+  console.info("cart++", cart)
 
-return (
-  <div className="content">
-         <h1>Products</h1>
-    <div className="App"></div>
-     <table border={1} >
+  return (
+    <div className="content">
+      <h1>Products</h1>
+      <div className="App">
+        <table border={1} >
           <thead>
-                 <tr>
-                      <th> Id</th>
-                      <th> Name</th>
-                      <th> Mobail no.</th>
-                      <th> Color</th>
-                 </tr>
+            <tr>
+              <th> Id</th>
+              <th> Name</th>
+              <th> Price</th>
+              <th> Quantity</th>
+              <th> Discripion</th>
+            </tr>
           </thead>
           <tbody>
             {
-              cart.map((i , index) => {
-                return(
+              cart.map((i, index) => {
+                return (
                   <tr key={index}>
-                    <td>{index+1}</td>
+                    <td>{index + 1}</td>
                     <td>{i?.name}</td>
                     <td>{i?.price}</td>
                     <td>{i?.quantity}</td>
@@ -38,14 +39,15 @@ return (
                 )
               })
             }
-         
+
           </tbody>
-     </table>
-     
-  </div>
-)
+        </table>
+      </div>
+
+    </div>
+  )
 
 }
 export default App;
- 
+
 
