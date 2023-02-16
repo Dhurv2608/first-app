61c62902813ae7ddaaf5a2d7d96b789dac8c8aa8
KDhruv2608 <dhruvkatrodiya1234@gmail.com>
2023-02-16 10:46:40 +0530

    show-data-arror

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-const App = () =>{
-  const data = [
 
+const app = () => {
+  const cart = [
+        {name: "mobail",price:13000,quantity:4,discripion:"best mobail qulity"},
+        {name: "tv",price:25000,quantity:3,discripion:"best tv qulity"},
+        {name: "freez",price:16000,quantity:2,discripion:"best frrez qulity"},
+        {name: "ac",price:5765,quantity:4,discripion:"best ac qulity"},
+        {name: "cloths",price:7543,quantity:9,discripion:"best cloths qulity"},
+        {name: "bike",price:36646,quantity:1,discripion:"best bike qulity"}
 ]
-}
+console.info("cart++",cart)
 
-function App() {
-  return (
-    <div className="App">
-       <table border={1} className="">
-            <thead>
-                   <tr>
-                        <th> Id</th>
-                        <th> Name</th>
-                        <th> Mobail no.</th>
-                        <th> Color</th>
-                   </tr>
-            </thead>
-            <tbody>
-                   <tr>
-                        <td>{i.index+1}</td>                   
-                   </tr>
-            </tbody>
-       </table>
-       
-    </div>
-  );
-}
+return (
+  <div className="content">
+         <h1>Products</h1>
+    <div className="App"></div>
+     <table border={1} >
+          <thead>
+                 <tr>
+                      <th> Id</th>
+                      <th> Name</th>
+                      <th> Mobail no.</th>
+                      <th> Color</th>
+                 </tr>
+          </thead>
+          <tbody>
+            {
+              cart.map((i , index) => {
+                return(
+                  <tr key={index}>
+                    <td>{index+1}</td>
+                    <td>{i?.name}</td>
+                    <td>{i?.price}</td>
+                    <td>{i?.quantity}</td>
+                    <td>{i?.discripion}</td>
+                  </tr>
+                )
+              })
+            }
+         
+          </tbody>
+     </table>
+     
+  </div>
+)
 
+}
 export default App;
+ 
+
