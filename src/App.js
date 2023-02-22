1e98f05eda385cf43a1e8cf262cde99baddff92a
Dhurv2608 <dhruvkatrodiya34@gmail.com>
2023-02-22 10:53:32 +0530

    complet view

--- src/App.js
+++ src/App.js
@@ -8,37 +8,56 @@ const App = () => {
     { name: "ac", price: 5765, quantity: 4, discripion: "best ac qulity" },
     { name: "cloths", price: 7543, quantity: 9, discripion: "best cloths qulity" },
     { name: "bike", price: 36646, quantity: 1, discripion: "best bike qulity" }
-  ]
-  console.info("cart++", cart)
-
+  ];
+  const xyz = cart.filter(a => a.quantity >= 3 );
+  const totalPrice = xyz.reduce((acc,curr) => {
+    return acc + curr.price
+  },0)
+  const totalquantity = xyz.reduce((acc,curr) => {
+    return acc + curr.quantity
+  },0)
+  const total = xyz.reduce((acc,curr) =>{
+            return acc + (curr.price * curr.quantity)
+  },0);
   return (
     <div className="content">
       <h1>Products</h1>
       <div className="App">
-        <table border={1} >
+        <table className="border" >
           <thead>
             <tr>
-              <th> Id</th>
-              <th> Name</th>
-              <th> Price</th>
-              <th> Quantity</th>
-              <th> Discripion</th>
+              <th className="border"> Id</th>
+              <th className="border"> Name</th>
+              <th className="border"> Discripion</th>
+              <th className="border"> Quantity</th>
+              <th className="border"> Price</th>
+               <th  className="border">Total</th>
             </tr>
+     
           </thead>
+          
           <tbody>
-            {
-              cart.map((i, index) => {
+              {
+              xyz.map((i, index) => {
                 return (
                   <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td>{i?.name}</td>
-                    <td>{i?.price}</td>
-                    <td>{i?.quantity}</td>
-                    <td>{i?.discripion}</td>
+                    <td className="border">{index + 1}</td>
+                    <td className="border">{i?.name}</td>
+                    <td className="border">{i?.discripion}</td>
+                    <td className="border">{i?.quantity}</td>
+                    <td className="border">{i?.price}</td>
+                    <td  className="border">{i?.price * i?.quantity}</td>
                   </tr>
+                  
                 )
               })
             }
+            <tr>
+                <td  className="border" colspan={3}>Total</td>
+                <td  className="border" >{totalquantity}</td>
+                <td  className="border" >{totalPrice}</td>
+                <td  className="border" >{total}</td>
+            </tr>
 
           </tbody>
         </table>
@@ -46,7 +65,6 @@ const App = () => {
 
     </div>
   )
-
 }
 export default App;
 
